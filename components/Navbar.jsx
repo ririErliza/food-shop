7794dda02afc7d6767ca192137688c60d86875b2
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-10 15:45:58 +0100

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Image from "next/image"
+import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+
+      <div className={styles.item}>
+
+        <div className={styles.callButton}>
+        <Image src="/images/call.png" alt="call" width="32" height="32"/>
+        </div>
+
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>0812 7902121</div>
+        </div>
+      
+      </div>
+
+      <div className={styles.item}>center</div>
+
+      <div className={styles.item}>right</div>
+      
+    </div>
   )
 }
 
